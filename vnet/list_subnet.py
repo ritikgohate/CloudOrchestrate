6be65362f5834b60da832abae2610f694493b3e3
ritikgohate <rlgohate247@gmail.com>
2025-09-01 14:25:06 +0000

    added vnet/list_subnet.py

--- vnet/list_subnet.py
+++ vnet/list_subnet.py
@@ -1,2 +1,7 @@
 from google.cloud import compute_v1
 from google.api_core.exceptions import NotFound
+
+def list_subnets(project_id, region):
+    client = compute_v1.SubnetworksClient()
+    for subnet in client.list(project=project_id, region=region):
+        print(f"Subnet: {subnet.name}, CIDR: {subnet.ip_cidr_range}")
